181778157aea153bba3500b275bbe8ba88e29ded
SebastianGonzalez <63818852+drixter4@users.noreply.github.com>
2024-02-06 16:10:02 -0600

    cambios de passport

--- src/middleware/Auth/authConfig.js
+++ src/middleware/Auth/authConfig.js
@@ -44,6 +44,7 @@ class PassportConfig {
         });
         passport_1.default.deserializeUser((id, done) => __awaiter(this, void 0, void 0, function* () {
             try {
+                console.log(id);
                 const user = yield User_1.default.findById(id);
                 if (!user) {
                     return done(null, false);
